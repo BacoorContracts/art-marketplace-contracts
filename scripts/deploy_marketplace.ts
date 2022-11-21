f131f6ccd351f28b1178b1fa150f91918673385b
ngduythao2805 <ngduythao2805@gmail.com>
2022-11-21 16:57:17 +0700

    Chore: deployment script

--- scripts/deploy_marketplace.ts
+++ scripts/deploy_marketplace.ts
@@ -3,8 +3,7 @@ import { ethers, upgrades } from "hardhat";
 
 async function main(): Promise<void> {
   const Logic: ContractFactory = await ethers.getContractFactory("Marketplace");
-  const logic: Contract = await upgrades.deployProxy(Logic, ["0x25433bACDa338631a9e8a493c4b280d94497f4Ae", "0xD39A527516d19b5cfddDd58A2544E52257a3323F"], { kind: "uups", initializer: "init" });
-  await logic.deployed();
+  const logic: Contract = await upgrades.deployProxy(Logic, ["0x25433bACDa338631a9e8a493c4b280d94497f4Ae", "0xD39A527516d19b5cfddDd58A2544E52257a3323F", ["0xfFE8597A65A56cdDF2B128547229176bED1a4540"], [1000], ["0xf2B0782C6C6bB5C375469E8151612E3b7b6cEa45"], [10000]], { kind: "uups", initializer: "init" });  await logic.deployed();
   console.log("Logic Proxy Contract deployed to : ", logic.address);
   console.log("Logic Contract implementation address is : ", await upgrades.erc1967.getImplementationAddress(logic.address));
 }
